0298cecc46f47d773dcb0ff35471d9d3d5acb06c
ayyush10 <ayu1234shgupta@gmail.com>
2022-10-26 21:40:00 +0530

    day4 commit

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'dart:html';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -17,14 +18,40 @@ class LoginPage extends StatelessWidget {
               "asset/image/loginpic.png",
               fit: BoxFit.cover,
             ),
-            SizedBox(
-              height: 20,
-            ),
             Text(
               "Welcome",
               style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16,horizontal:32 ),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "enter user name",
+                      labelText: "username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "enter user name",
+                      labelText: "username",
+                    ),
+                  ),
+                  SizedBox(
+              height: 20.0,),
+             ElevatedButton(onPressed: (){print("Hi Computer",)},
+              child: Text("Login"),
+              style: TextButton.styleFrom()
+              ,) 
+                ],
               ),
             )
           ],
